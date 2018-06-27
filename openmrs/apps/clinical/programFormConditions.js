6b45064400416f494c33d6e6185d1ca7e7a571fa
Ian Manyama <iannashin@gmail.com>
2018-06-27 14:47:52 +0300

    Ian Manyama | Cleanup/Merging v2

--- openmrs/apps/clinical/programFormConditions.js
+++ openmrs/apps/clinical/programFormConditions.js
@@ -17,5 +17,39 @@ Bahmni.Clinical.Program.FormConditions.rules = {
             conditions.hide.push('Name of Community Support Organisation');
         }
         return conditions;
+    },
+    'Transfer In': function (patientProgramAttributes, patient) {
+        var conditions = {
+            show: [],
+            hide: []
+        };
+
+
+    if (typeof something === "undefined") {
+    {
+        if(patient.gender == 'M')
+        {
+         conditions.hide.push('Breast Feeding');  
+         conditions.hide.push('Pregnant');  
+        }else
+        {
+              conditions.show.push('Breast Feeding');
+              conditions.show.push('Pregnant');
+        }
+       }
+        if (patientProgramAttributes['Transfer In'] == 'WITH RECORDS' ||patientProgramAttributes['Transfer In'] == 'NO RECORDS AVAILABLE' || patientProgramAttributes['Transfer In'] == 'IN CARE' || patientProgramAttributes['Transfer In'] == 'ON ART' ) {
+            conditions.show.push('WHO Stage');  
+            conditions.show.push('Prior ARV Exposure');     
+            conditions.show.push('Date Ready to Start ARV');    
+            conditions.show.push('Date Start ARV');     
+            conditions.show.push('CD4 Count');    
+        } else  {
+             conditions.hide.push('WHO Stage');
+              conditions.hide.push('Prior ARV Exposure');
+              conditions.hide.push('Date Ready to Start ARV'); 
+              conditions.hide.push('Date Start ARV');  
+              conditions.hide.push('CD4 Count');   
+        }
+        return conditions;
     }
 };
