e234e450bacbe190b0e91ae12fb9edef0ff61352
Ian Manyama <iannashin@gmail.com>
2018-06-28 11:41:40 +0300

    Ian Manyama | Cleanup/Merging v3

--- openmrs/apps/clinical/programFormConditions.js
+++ openmrs/apps/clinical/programFormConditions.js
@@ -1,34 +1,12 @@
 Bahmni.Clinical.Program.FormConditions.rules = {
-    'DOT Option': function (patientProgramAttributes) {
+ 'Transfer In': function (patientProgramAttributes, patient) {
         var conditions = {
             show: [],
             hide: []
         };
-        if (patientProgramAttributes['DOT Option'] == 'Home-based DOT') {
-
-            conditions.show.push('Name of Treatment Supporter');
-            conditions.show.push('Tel No of Treatment Supporter');
-            conditions.show.push('Physical Address of Treatment Supporter');
-            conditions.show.push('Name of Community Support Organisation');
-        } else if (patientProgramAttributes['DOT Option'] == 'Health Facility DOT') {
-            conditions.hide.push('Name of Treatment Supporter');
-            conditions.hide.push('Tel No of Treatment Supporter');
-            conditions.hide.push('Physical Address of Treatment Supporter');
-            conditions.hide.push('Name of Community Support Organisation');
-        }
-        return conditions;
-    },
-    'Transfer In': function (patientProgramAttributes, patient) {
-        var conditions = {
-            show: [],
-            hide: []
-        };
-
-
-    if (typeof something === "undefined") {
-    {
+     if (typeof patient !== "undefined") {
         if(patient.gender == 'M')
-        {
+          {
          conditions.hide.push('Breast Feeding');  
          conditions.hide.push('Pregnant');  
         }else
@@ -36,7 +14,8 @@ Bahmni.Clinical.Program.FormConditions.rules = {
               conditions.show.push('Breast Feeding');
               conditions.show.push('Pregnant');
         }
-       }
+      }
+
         if (patientProgramAttributes['Transfer In'] == 'WITH RECORDS' ||patientProgramAttributes['Transfer In'] == 'NO RECORDS AVAILABLE' || patientProgramAttributes['Transfer In'] == 'IN CARE' || patientProgramAttributes['Transfer In'] == 'ON ART' ) {
             conditions.show.push('WHO Stage');  
             conditions.show.push('Prior ARV Exposure');     
@@ -49,7 +28,7 @@ Bahmni.Clinical.Program.FormConditions.rules = {
               conditions.hide.push('Date Ready to Start ARV'); 
               conditions.hide.push('Date Start ARV');  
               conditions.hide.push('CD4 Count');   
-        }
+             }
+
         return conditions;
-    }
-};
+}};
