b0eb3ca7dfb15b61a9c91c1cb7618e20c5b1395b
Ian Manyama <iannashin@gmail.com>
2018-07-24 12:21:49 +0300

    Ian Manyama | Cleanup

---
+++ openmrs/apps_bk/clinical/programFormConditions.js
@@ -0,0 +1,43 @@
+Bahmni.Clinical.Program.FormConditions.rules = {
+ 'Transfer In': function (patientProgramAttributes, patient) {
+        var conditions = {
+            show: [],
+            hide: []
+        };
+     if (typeof patient !== "undefined") {
+        if(patient.gender == 'M')
+          {
+         conditions.hide.push('Breast Feeding');  
+         conditions.hide.push('Pregnant');  
+        }else
+        {
+              conditions.show.push('Breast Feeding');
+              conditions.show.push('Pregnant');
+        }
+      }
+
+        if (patientProgramAttributes['Transfer In'] == 'WITH RECORDS' ||
+             patientProgramAttributes['Transfer In'] == 'IN CARE' ) {
+            conditions.show.push('WHO Stage');  
+            conditions.show.push('Prior ARV Exposure');     
+            conditions.show.push('CD4 Count');    
+            conditions.hide.push('Date Ready to Start ARV');    
+            conditions.hide.push('Date Start ARV');  
+        } else if (patientProgramAttributes['Transfer In'] == 'ON ART')
+        {            
+            conditions.show.push('Date Ready to Start ARV');    
+            conditions.show.push('Date Start ARV'); 
+            conditions.show.push('WHO Stage');  
+            conditions.show.push('Prior ARV Exposure');     
+            conditions.show.push('CD4 Count');   
+
+        } else  {
+             conditions.hide.push('WHO Stage');
+              conditions.hide.push('Prior ARV Exposure');
+              conditions.hide.push('Date Ready to Start ARV'); 
+              conditions.hide.push('Date Start ARV');  
+              conditions.hide.push('CD4 Count');   
+             }
+
+        return conditions;
+}};
